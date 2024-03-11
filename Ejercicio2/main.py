a916e00dec6fda9c10323e159be8f89e04a9370d
RomoDavid21 <romo.david@uabc.edu.mx>
2024-03-11 11:42:06 -0700

    Ejercicio 2

---
+++ Ejercicio2/main.py
@@ -0,0 +1,19 @@
+
+ciudad = input("Ingresa una ciudad: ")
+sustantivo = input("Ingresa un sustantivo: ")
+verbo = input("Ingresa un verbo: ")
+adjetivo1 = input("Ingresa un adjetivo: ")
+nombrePropio = input("Ingresa un nombre propio: ")
+sustantivoPlural = input("Ingresa un sustantivo plural: ")
+verboPasado = input("Ingresa un verbo en pasado: ")
+adjetivo2 = input("Ingresa un adjetivo: ")
+animal = input("Ingresa el nombre de un animal en plural: ")
+lugar = input("Ingresa el nombre de un lugar: ")
+
+
+print("\nEn la bulliciosa ciudad de ",ciudad," se encontraba un curioso ",sustantivo," que siempre soñaba ")
+print("con ",verbo,". Con su",adjetivo1," y su valentía, nuestro héroe, conocido como ",nombrePropio,", se ")
+print("embarcó en una emocionante aventura llena de ",sustantivoPlural," y misterios.")
+print("\nDurante su viaje, se encontró con un grupo de ",animal," que lo ayudaron a ",verboPasado," en ")
+print("situaciones complicadas. Juntos vivieron momentos ",adjetivo2," y descubrieron la magia que se ")
+print("escondía detrás de cada ",lugar,".")
