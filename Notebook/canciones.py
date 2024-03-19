12c013defe65f8aa049d321406e362b56ee152ba
RomoDavid21 <romo.david@uabc.edu.mx>
2024-03-18 18:50:11 -0700

    Notebook de los 11 ejercicios de Python

---
+++ Notebook/canciones.py
@@ -0,0 +1,271 @@
+Bones="""
+Gimme gimme gimme some time to think
+I’m in the bathroom looking at me
+Face in the mirror is all I need
+Wait until the reaper takes my life
+Never gonna get me out alive
+I will live a thousand million lives
+
+My patience is waning
+Is this entertaining?
+My patience is waning
+Is this entertaining?
+
+I got this feeling yeah you know
+Where I’m losing all control
+Cause there’s magic in my bones
+I got this feeling in my soul
+Go ahead and throw your stones
+Cause there’s magic in my bones
+
+Playing with a stick of dynamite
+There was never grey in black and white
+There was never wrong till there was right
+Feeling like a boulder hurdling
+Seeing all the vultures circling
+Burning in the flames I’m working in
+Turning in a bed that’s darkening
+
+My patience is waning
+Is this entertaining?
+My patience is waning
+Is this entertaining?
+
+I got this feeling yeah you know
+Where I’m losing all control
+Cause there’s magic in my bones
+I got this feeling in my soul
+Go ahead and throw your stones
+Cause there’s magic in my bones
+
+Cause there’s magic in my bones
+
+Look in the mirror of my mind
+Turning the pages of my life
+Walking the path so many paced a million times
+Drown out the voices in the air
+Leaving the ones that never cared
+Picking the pieces up and building to the sky
+
+My patience is waning
+Is this entertaining?
+My patience is waning
+Is this entertaining?
+
+I got this feeling yeah you know
+Where I’m losing all control
+Cause there’s magic in my bones
+I got this feeling in my soul
+Go ahead and throw your stones
+Cause there’s magic in my bones
+
+There goes my mind, don’t mind
+There goes my mind
+There goes my mind, don’t mind
+There goes my mind
+Cause there’s magic in my bones
+"""
+
+Enemy = """
+Look out for yourself
+
+I wake up to the sounds of the silence that allows
+For my mind to run around, with my ear up to the ground
+I’m searching to behold the stories that are told
+When my back is to the world, that was smiling when I turned
+Tell you you’re the greatest
+But once you turn they hate us
+
+Oh, the misery
+Everybody wants to be my enemy
+Spare the sympathy
+Everybody wants to be my enemy
+Look out for yourself
+My enemy
+Look out for yourself
+But I’m ready
+
+Your words up on the wall, as you’re praying for my fall
+And the laughter in the halls, and the names that I’ve been called
+I stack it in my mind, and I’m waiting for the time
+When I show you what it’s like, to be words spit in a mic
+Tell you you’re the greatest
+But once you turn they hate us
+
+Oh, the misery
+Everybody wants to be my enemy
+Smell the sympathy
+Everybody wants to be my enemy
+Look out for yourself
+My enemy
+Look, look, look, look, look out for yourself
+
+_JID verse_
+
+Yeah
+Uh, look, okay
+I’m hoping that somebody pray for me
+I’m praying that somebody hope for me
+I’m staying where nobody ‘posed to be
+P-p-posted
+Being a wreck of emotions
+Ready to go whenever just let me know
+The road is long so put the pedal into the floor
+The enemy’s on my trail
+My energy unavailable
+Imma tell ‘em hasta luego
+They wanna plot on my trot to the top
+I’ve been outta shape
+Thinkin’ out the box
+I’m an astronaut
+I blasted off the planet rock to cause Catastrophe and it matters, more because I had it not,
+Had I  thought about wreaking havoc on an opposition
+Kinda shockin’ they wanted static 
+With precision I’m automatic quarterback
+I ain’t talkin’ sacking
+Pack it pack it up
+I don’t panic
+Batter batter up
+Who the baddest
+It don’t matter
+Cause we at ya throat
+
+__
+
+Everybody wants to be my enemy
+Spare the sympathy
+Everybody wants to be my enemy
+Oh, the misery
+Everybody wants to be my enemy
+Spare the sympathy
+Everybody wants to be my enemy
+
+They say pray it away, I swear, I’ll never be a Saint, no way
+My enemy
+They say pray it away, I swear, I’ll never be a Saint
+Look out for yourself
+"""
+
+SigoAqui = """
+Soy
+La pregunta del millón
+Siempre la interrogación
+No respondas
+Que sí, porque sí
+¿Y qué?
+¿Qué podrías tú decir?
+Si yo no te voy a oír
+No me entiendes
+Y nunca seré lo que esperas de mí
+Jamás
+Ya me vas a conocer
+Niño y hombre puedo ser
+No me uses
+Y apartes de ti
+Y di
+¿Cómo alguien aprendió?
+Lo que nadie le enseñó
+No me entienden
+No estoy aquí
+Y yo solo quiero ser real
+Y sentir el mundo igual
+Que los otros
+Seguir siempre así
+¿Por qué yo tendría que cambiar?
+Nadie más lo va a intentar
+Y no entienden
+Que sigo aquí
+Y tú
+Ves lo que ellos nunca ven
+Te daría el cien por cien
+Me conoces
+Y ya no hay temor
+Yo
+Mostraría lo que soy
+Si tú vienes donde voy
+No me alcanzan
+Si eres mi amigo mejor
+¿Qué sabrán del mal y el bien?
+Yo no soy lo que ven
+Todo un mundo durmiendo, y yo sigo soñando, ¿por qué?
+Sus palabras susurran mentiras que nunca creeré
+Y yo solo quiero ser real
+Y sentir el mundo igual
+Que los otros
+Por ellos, por mí
+¿Por qué yo tendría que cambiar?
+Nadie más lo va a intentar
+Estoy solo
+Y sigo aquí
+Solo yo
+Estoy aquí
+Sigo aquí
+Sigo aquí
+"""
+
+What = """
+I am the monster you created
+You ripped out all my parts
+And worst of all, for me to live, I gotta kill the part of me that saw
+That I needed you more
+I hope you know we had everything
+And you broke me and left these pieces
+I want you to hurt like you hurt me today and
+I want you to lose like I lose when I play what could have been
+Oh, what could have been
+Why don't you love who I am?
+What we could have been
+I am your ghost, a fallen angel
+You ripped out all my parts
+I couldn't care what invention you made me
+'Cause I, I was meant to be yours
+I hope you know we had everything
+And you broke me and left these pieces
+I want you to hurt like you hurt me today and
+I want you to lose like I lose when I play
+I want you to hurt like you hurt me today and
+I want you to lose like I lose when I play
+What could have been
+"""
+
+Slave = """
+I wanna be your slave, I wanna be your master
+I wanna make your heartbeat run like rollercoasters
+I wanna be a good boy, I wanna be a gangster
+'Cause you could be the beauty and I could be the monster
+I love you since this morning, not just for aesthetic
+I wanna touch your body, so fucking electric
+I know you're scared of me, you say that I'm too eccentric
+I'm crying all my tears and that's fucking pathetic
+I wanna make you hungry, then I wanna feed ya
+I wanna paint your face like you're my Mona Lisa
+I wanna be a champion, I wanna be a loser
+I'll even be a clown 'cause I just wanna amuse ya
+I wanna be your sex toy, I wanna be your teacher
+I wanna be your sin, I wanna be your preacher
+I wanna make you love me, then I wanna leave ya
+'Cause, baby, I'm your David and you're my Goliath
+Uh-huh
+Mhm, uh-huh
+Because I'm the devil who's searching for redemption
+And I'm a lawyer who's searching for redemption
+And I'm a killer who's searching for redemption
+A motherfucking monster who's searching for redemption
+And I'm a bad guy who's searching for redemption
+And I'm a blonde girl who's searching for redemption
+And I'm a freak that is searching for redemption
+A motherfucking monster who's searching for redemption
+I wanna be your slave, I wanna be your master
+I wanna make your heartbeat run like rollercoasters
+I wanna be a good boy, I wanna be a gangster
+'Cause you can be the beauty and I could be the monster
+I wanna make you quiet, I wanna make you nervous
+I wanna set you free but I'm too fucking jealous
+I wanna pull your strings like you're my Telecaster
+And if you want to use me, I could be your puppet
+'Cause I'm the devil who's searching for redemption
+And I'm a lawyer who's searching for redemption
+And I'm a killer who's searching for redemption
+A motherfucking monster who's searching for redemption
+I wanna be your slave, I wanna be your master
+"""
